42b3f7ff8a38487b4385fa2ca96f5d5a22d1c540
Manuchi06 <mburiticg1@eafit.edu.co>
2026-08-27 15:12:55 -0500

    Update fb_bruteforce.hpp

--- src/fb_bruteforce.hpp
+++ src/fb_bruteforce.hpp
@@ -4,15 +4,13 @@
 #include <string>
 #include <cstdint>
 
-// Estructura para retornar los resultados del módulo FB
 struct ResultadoFB {
     std::string candidato;
     uint64_t evaluaciones;
     std::string estado;
 };
 
-// Declaración de funciones
 ResultadoFB FuerzaBruta(const std::string& hash_objetivo, const std::string& alfabeto, int longitud);
-ResultadoFB AtaqueDiccionario(const std::string& hash_objetivo, const std::string& ruta_diccionario);
+ResultadoFB AtaqueDiccionario(const std::string& hash_objetivo,const std::string& ruta_diccionario);
 
-#endif // FB_BRUTEFORCE_HPP
+#endif
